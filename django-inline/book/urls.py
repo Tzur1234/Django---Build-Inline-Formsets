bb2476ef684c4793b764e48b61d47c5b916f0093
root <root@DESKTOP-O1KC17T.localdomain>
2023-02-13 23:39:37 +0200

    Create AuthorListView

--- django-inline/book/urls.py
+++ django-inline/book/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 app_name = 'books'
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='books')
+    path('', views.HomeView.as_view(), name='home'),
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
+    path('authors/add/', views.AuthorCreateView.as_view(), name='add_author')
 ]
