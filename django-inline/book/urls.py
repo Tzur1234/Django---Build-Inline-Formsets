c02df7ac54d91d49bcb97c766a69d642116d0b54
root <root@DESKTOP-O1KC17T.localdomain>
2023-02-14 16:34:40 +0200

    Add UpdateView to update Author's name

--- django-inline/book/urls.py
+++ django-inline/book/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('authors/', views.AuthorListView.as_view(), name='authors'),
     path('authors/add/', views.AuthorCreateView.as_view(), name='add_author'),
-    path('authors/<int:pk>/', views.AuthorDetailView.as_view(), name='author_detail')
+    path('authors/<int:pk>/', views.AuthorDetailView.as_view(), name='author_detail'),
+    path('authors/<int:pk>/books/edit', views.AuthorBooksEditView.as_view(), name='author_book_edit')
 ]
