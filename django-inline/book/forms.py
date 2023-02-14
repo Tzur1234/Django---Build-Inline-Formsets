0ed2971f402d7fa0f37935732a1f3bef1c7c1586
root <root@DESKTOP-O1KC17T.localdomain>
2023-02-14 17:55:48 +0200

    Creating the view (SingleObjectMixin, FormView) + start using inline_factory

---
+++ django-inline/book/forms.py
@@ -0,0 +1,7 @@
+from django.forms import ModelForm
+
+from django.forms.models import inlineformset_factory
+
+from .models import Author, Book
+
+AuthorBooksFormset = inlineformset_factory(Author, Book, fields=('name',))
